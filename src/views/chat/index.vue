0a520e276ad0422fdb83ff5bccb047d928de3bdc
leo-pc <LeoAlanHou@gmail.com>
2023-03-09 21:46:21 +0800

    🌟小Tips:用英文提问的回复质量一般会更高🌟

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -432,7 +432,7 @@ onUnmounted(() => {
           <template v-if="!dataSources.length">
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-              <span>Aha~</span>
+              <span>🌟小Tips:用英文提问的回复质量一般会更高🌟</span>
             </div>
           </template>
           <template v-else>
